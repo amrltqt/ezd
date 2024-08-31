3e2630845a49d65ca253cd3cf2b7ee86a488694f
Quentin Amirault <amirault.quentin@gmail.com>
2024-08-31 12:22:38 +0200

    chore: remove usuned dependencies

--- sdk/src/ezd/targets/slack.py
+++ sdk/src/ezd/targets/slack.py
@@ -1,17 +1,15 @@
-from pydantic import BaseModel
-from typing import Union, Literal
-
+from typing import Literal
 
 from ezd.core import DistributionTarget
 
 class SlackChannel(DistributionTarget):
-    type: Literal["slack-user"]  = "slack-user"
+    type: Literal["slack-user"] = "slack-user"
     channel: str
     filename: str
     comment: str
 
 class SlackUser(DistributionTarget):
-    type: Literal["slack-channel"]  = "slack-channel"
+    type: Literal["slack-channel"] = "slack-channel"
     user: str
     filename: str
     comment: str
